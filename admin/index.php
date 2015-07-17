4c2add5af6796394effcf0643f4549d31620e468
flojd <loncar.danimir@gmail.com>
2015-07-17 16:05:44 +0200

    admin-index.php done for now

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<?php include 'includes/header.php'; ?>
+
+            
+<table class="table table-striped">
+    <tr>
+        <th>Post #id</th>
+        <th>Post Title</th>
+        <th>Category</th>
+        <th>Author</th>
+        <th>Date</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+
+<table class="table table-striped">
+    <tr>
+        <th>Category #id</th>
+        <th>Category Name</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+            
+            
+
+<?php include 'includes/footer.php'; ?>
